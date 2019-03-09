691a73aa69249f5b58f00b518bbd09d2ddd460c2
xmy7080 <ximuyuan1989@gmail.com>
2019-03-08 21:48:35 -0800

    Update TrappingRainWater.py

--- TrappingRainWater.py
+++ TrappingRainWater.py
@@ -1,3 +1,4 @@
+#=======two pointers========
 class Solution(object):
     def trap(self, height):
         """
@@ -19,4 +20,25 @@ class Solution(object):
                     vol += hr - height[r]
                 r -=1
         return vol
+#=======stack approach========
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        # stk = [ (height[0], 0) ]
+        water = 0
+        stk = []
+        for cur in range(len(height)):
+            while len(stk) > 1 and height[cur] > height[ stk[len(stk)-1] ]:
+                top = stk.pop()
+                boundDistance = cur - stk[len(stk)-1] - 1
+                boundHeight = min(height[cur], height[ stk[len(stk)-1] ]) - height[top]
+                if boundHeight < 0: break
+                water += boundDistance * boundHeight
+            stk.append(cur)
+        
+        return water
+                
                     
