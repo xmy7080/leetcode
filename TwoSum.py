2aa58c406112fcb0aaee4e5c31ace20730ca8d69
xmy7080 <ximuyuan1989@gmail.com>
2020-03-13 22:21:42 -0700

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -14,3 +14,21 @@ class Solution(object):
             if nums[i] not in dic and target-nums[i] in dic:
                 return [dic[target-nums[i]],i]
             dic[nums[i]] = i
+            
+            
+            
+=====better way with enumerate===
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i, n in enumerate(nums):
+            if target - n in dic:
+                return [dic[target-n], i]
+            else:
+                dic[n] = i
+            
