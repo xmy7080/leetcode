5528ef4813aa9ba3f09a888aabc8f0b9cbfaf7ca
xmy7080 <ximuyuan1989@gmail.com>
2020-03-18 15:44:07 -0700

    Update MaximumProductSubarray.py

--- MaximumProductSubarray.py
+++ MaximumProductSubarray.py
@@ -1,3 +1,21 @@
+#===jiuzhang dp exmaple approach, using 1 dimensional array, fast but O(n) space====
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxl = [0] * len(nums)
+        minl = [0] * len(nums)
+        maxl[0], minl[0] = nums[0], nums[0]
+        for i, n in enumerate(nums):
+            if i == 0:
+                continue
+            maxl[i] = max(n, max(n * maxl[i-1], n * minl[i-1]) )
+            minl[i] = min(n, min(n * maxl[i-1], n * minl[i-1]) )
+        return max(maxl)
+    
+#===optimized for rolling array (1 demensional array collapse to a single number)====
 class Solution(object):
     def maxProduct(self, nums):
         """
