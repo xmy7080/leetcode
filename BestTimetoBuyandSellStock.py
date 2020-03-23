f167ff42b7d002322b8ad52cba15afef37adc3c4
xmy7080 <ximuyuan1989@gmail.com>
2020-03-22 21:18:36 -0700

    Update BestTimetoBuyandSellStock.py

--- BestTimetoBuyandSellStock.py
+++ BestTimetoBuyandSellStock.py
@@ -1,4 +1,22 @@
+# simple DP solution ===O(1) space and O(n) time ====
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        low = prices[0]
+        ans = 0
+        for p in prices:
+            if p < low:
+                low = p
+                continue
+            ans = max(ans, p - low)
+        return ans
 
+#======old solution with same time space complexity
 class Solution(object):
     def maxProfit(self, prices):
         """
