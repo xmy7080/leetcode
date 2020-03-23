85060f5da00d78966566facab85d65bc460577e1
xmy7080 <ximuyuan1989@gmail.com>
2020-03-22 22:50:50 -0700

    Update BestTimetoBuyandSellStockIII.py

--- BestTimetoBuyandSellStockIII.py
+++ BestTimetoBuyandSellStockIII.py
@@ -1,3 +1,30 @@
+#======dp solution from jiuzhang, set 5 states (before 1st buy, during 1st holding, after 1 st holding but before 2nd buy, during and after 2nd holding
+#, and maintain a [n][5] array keep those state==
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices: return 0
+        dp = [[0] * 6 for i in xrange(len(prices)+1)]
+        
+        for i in xrange(1, len(prices)+1):
+            #phase 1,3,5, 
+            for j in [1,3,5]:
+                dp[i][j] = dp[i-1][j] #case keep prev day state
+                if i >1 and j > 1:
+                    #case start new state, need add prev day gain. also prices[i-1] is the price on day i, cause i started at 1
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + prices[i-1] - prices[i-2])
+            
+            #phase 2,4
+            for j in [2,4]:
+                dp[i][j] = dp[i-1][j-1] #case just start new state, no gain yet
+                if i >1:
+                    #case keep prev state, accumualating gain every day
+                    dp[i][j] = max(dp[i][j], dp[i-1][j] + prices[i-1] - prices[i-2]) 
+        return max(dp[len(prices)][3], dp[len(prices)][5])
+#======
 class Solution(object):
     def maxProfit(self, prices):
         """
