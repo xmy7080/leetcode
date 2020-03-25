c679df70ee6227058920531c381dadc056df5f4c
xmy7080 <ximuyuan1989@gmail.com>
2020-03-24 22:26:01 -0700

    Update LongestIncreasingSubsequence.py

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -1,3 +1,19 @@
+#======new O(n^2) solution, from jiuzhang dp 坐标型=====
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not len(nums): return 0
+        dp = [0] * len(nums)
+        for i in xrange(len(nums)):
+            dp[i] = 1
+            for j in xrange(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+#======old O(n^2) solution===
 class Solution(object):
     def lengthOfLIS(self, nums):
         """
