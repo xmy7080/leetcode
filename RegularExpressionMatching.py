c9a5966c22401ad5b92ef2f013f1c23eccb6a8da
xmy7080 <ximuyuan1989@gmail.com>
2020-03-30 17:29:52 -0700

    Update RegularExpressionMatching.py

--- RegularExpressionMatching.py
+++ RegularExpressionMatching.py
@@ -1,3 +1,22 @@
+#===leetcode solution, second dp approach
+#https://leetcode.com/articles/regular-expression-matching/
+class Solution(object):
+    def isMatch(self, s, p):
+        sl, pl = len(s), len(p)
+        dp = [[False] * (pl + 1) for _ in xrange(sl+1)]
+        
+        dp[-1][-1] = True
+        #i and j all matches to idx in string, hence 0 based
+        for i in xrange(sl, -1, -1):
+            for j in xrange(pl-1, -1, -1):
+                first_match = i < sl and p[j] in (s[i], '.')
+                if j+1< pl and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or (first_match and dp[i+1][j])
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
+        return dp[0][0]
+    
+#====my own solution, not sure the reasoning
 class Solution(object):
     def isMatch(self, s, p):
         m, n = len(s), len(p)
