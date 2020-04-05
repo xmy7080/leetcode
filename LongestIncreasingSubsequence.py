6a6729794a091189153367ac6835fde3022b5406
xmy7080 <ximuyuan1989@gmail.com>
2020-04-05 16:23:34 -0700

    Update LongestIncreasingSubsequence.py

--- LongestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence.py
@@ -1,3 +1,34 @@
+#======O(n logn) solution, jiuzhang dp lesson 7
+class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, A):
+        # write your code here
+        if not A: return 0
+        #f: 0 1 2 3 4
+        #a: -inf 10 20 30 ...
+        B = [0] * (len(A)+1)
+        Btop = 0
+        B[0] = -sys.maxint-1
+        start, stop, mid, last = 0,0,0,0
+        for i in xrange(len(A)):
+            #find in B the biggest number that is smaller than A[i]
+            start, stop = 0, Btop
+            while(start <= stop):
+                mid = (start + stop)/2
+                if B[mid] < A[i]:
+                    last = mid #save mid as one candidate
+                    start = mid + 1
+                else:
+                    stop = mid -1
+            
+            B[last + 1] = A[i]
+            #when B[] are growing, Btop += 1
+            if last + 1 > Btop:
+                Btop += 1
+        return Btop
 #======new O(n^2) solution, from jiuzhang dp 坐标型=====
 class Solution(object):
     def lengthOfLIS(self, nums):
