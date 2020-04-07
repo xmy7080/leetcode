08e9bd7cacd2f7d3a1824a9ebe77994cdc172d13
xmy7080 <ximuyuan1989@gmail.com>
2020-04-06 21:43:39 -0700

    Update WildcardMatching.py

--- WildcardMatching.py
+++ WildcardMatching.py
@@ -1,3 +1,42 @@
+#=====jiuzhang two subsequence solution lesson 6====
+class Solution:
+    """
+    @param s: A string 
+    @param p: A string includes "?" and "*"
+    @return: is Match?
+    """
+    #s[m-1] == p[n-1] or p[n-1] == '?'
+    #dp[i][j] = dp[i-1][j-1]
+    
+    #p[n-1] == '*', cases are matched a char in s or not matched at all
+    #dp[i][j] = dp[i-1][j] or dp[i][j-1]
+    def isMatch(self, s, p):
+        # write your code here
+        if not s and not p: return True
+        if not p: return False
+        ls, lp = len(s), len(p)
+        #dp[0-ls][0-lp]
+        dp = [[False] * (lp+1) for _ in xrange(ls+1)]
+        
+        for i in xrange(ls+1):
+            for j in xrange(lp+1):
+                if not i and not j:
+                    dp[i][j] = True
+                    continue
+                if not j:
+                    dp[i][j] = False
+                    continue
+                #when j != 0 , i could be 0
+                if p[j-1] != '*':
+                    if i > 0 and p[j-1] in ('?', s[i-1]):
+                        dp[i][j] = dp[i-1][j-1]
+                else: #p[j-1] == '*'
+                    dp[i][j] = dp[i][j-1]
+                    if i > 0:
+                        dp[i][j]  = dp[i][j] or dp[i-1][j]
+        
+        return dp[ls][lp]
+===========
 class Solution(object):
     def isMatch(self, s, p):
         
