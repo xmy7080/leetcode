5de08553d9a7492d0b59470df84664bebee3a00e
xmy7080 <ximuyuan1989@gmail.com>
2020-04-30 10:25:56 -0700

    Update GroupAnagrams.py

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,3 +1,19 @@
+#faster way of put counts as a tuple in the dic key
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic = collections.defaultdict(list)
+        for str in strs:
+            count = [0] * 26
+            for c in str:
+                count[ord(c) - ord('a') ] += 1
+            dic[tuple(count) ].append(str)
+        return dic.values()
+    
+#original way or sorting the strings
 class Solution(object):
     def groupAnagrams(self, strs):
         """
