d6e080e75f17efd36f1b33750d44d7b9232dc307
xmy7080 <ximuyuan1989@gmail.com>
2020-05-01 19:24:37 -0700

    Update LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,3 +1,19 @@
+#leetcode solution
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs: return ""
+        elif len(strs) == 1: return strs[0]
+        for i in xrange(len(strs[0])):
+            char = strs[0][i]
+            for s in strs[1:]:
+                if len(s) == i or s[i] != char:
+                    return s[:i]
+        return strs[0]
+#old approach
 class Solution(object):
     def longestCommonPrefix(self, strs):
         """
