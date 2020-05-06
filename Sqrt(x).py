9825ad07a910331d992cdce8646a901d17fd0b2d
xmy7080 <ximuyuan1989@gmail.com>
2020-05-06 10:55:49 -0700

    Update Sqrt(x).py

--- Sqrt(x).py
+++ Sqrt(x).py
@@ -1,3 +1,26 @@
+#implementation that can return float, percision to 0.01
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: float
+        :rtype: float
+        """
+        if x in (0,1): return x
+        e = 0.01
+        l, r = float(1), float(x)
+        if x <1:
+            tmp = r
+            r = l
+            l = tmp
+        while l < r:
+            mid = l + (r-l)/2
+            if mid < (x - e)/ mid:
+                l = mid
+            elif mid > (x + e)/ mid:
+                r = mid
+            else:
+                return mid
+#leetcode original question, only return integer
 class Solution(object):
     def mySqrt(self, x):
         """
