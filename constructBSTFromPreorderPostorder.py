20b9c70b736bce18ca372f33a4161a3d0a4fe64d
xmy7080 <ximuyuan1989@gmail.com>
2020-05-07 16:26:21 -0700

    Update constructBSTFromPreorderPostorder.py

--- constructBSTFromPreorderPostorder.py
+++ constructBSTFromPreorderPostorder.py
@@ -1,10 +1,44 @@
 #pony ai phone interview, lt 889
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#lt concise solution
+class Solution(object):
+    def constructFromPrePost(self, pre, post):
+        if not pre: return None
+        root = TreeNode(pre[0])
+        if len(pre) == 1: return root
+
+        L = post.index(pre[1]) + 1
+        root.left = self.constructFromPrePost(pre[1:L+1], post[:L])
+        root.right = self.constructFromPrePost(pre[L+1:], post[L:-1])
+        return root
+#my actual answer during interview
+"""
+
+Given  2 strings:
+pre-order:  ABCDE
+post-order: CDBEA
+
+illustration:
+     A
+    / \
+    B  E
+   / \ /\
+   C D F G
+
+    A
+   / \
+     B
+    /\
+    C D
+
+A B CD
+CD B A
+1. Construct a binary tree satisfying the two conditions?
+a b cd e fg
+cdb fg e a
+
+b c d
+c d b
+"""
 class Solution(object):
     def constructFromPrePost(self, pre, post):
         """
