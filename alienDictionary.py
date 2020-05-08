186b8879139b50751ef97899669337fbb0eb12d5
xmy7080 <ximuyuan1989@gmail.com>
2020-05-08 13:40:21 -0700

    Update alienDictionary.py

--- alienDictionary.py
+++ alienDictionary.py
@@ -1,3 +1,36 @@
+#latest leetcode answer
+#pay attention to the counter usage, it could be replaced by a dict, which is parent class of Counter
+from collections import defaultdict, Counter, deque
+
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adj = defaultdict(set)
+        inCount = Counter({c: 0 for word in words for c in word} )
+        #inCount = dict({c: 0 for word in words for c in word} ) will also work
+        
+        for pair in zip(words, words[1:]):
+            for a, b in zip(*pair):
+                if a != b:
+                    if b not in adj[a]:
+                        adj[a].add(b)
+                        inCount[b] += 1
+                    break
+            #check if there are 'abcd', 'ab' for adjacent pairs, cause if a prefix 
+            #appears behind a word, there won't be a alphabetic order genereated
+            if len(pair[0]) > len(pair[1]) and pair[0].startswith(pair[1]): return ""
+        ans = []
+        queue = deque([c for c in inCount if inCount[c] == 0])
+        while queue:
+            c = queue.popleft()
+            ans.append(c)
+            for ne in adj[c]:
+                inCount[ne] -= 1
+                if not inCount[ne]:
+                    queue.append(ne)
+        if len(ans) != len(inCount):
+            return "" #contains cycle
+        return "".join(ans)
+#old leetcode answer
 class Node(object):
     def __init__(self):
         self.IN = set()
