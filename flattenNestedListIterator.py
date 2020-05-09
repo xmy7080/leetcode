ef1b07dc65534f1d8c1ab4ad8b94dd3a4e5439a5
xmy7080 <ximuyuan1989@gmail.com>
2020-05-09 13:51:05 -0700

    Create flattenNestedListIterator.py

---
+++ flattenNestedListIterator.py
@@ -0,0 +1,26 @@
+#Idea is, first flatten the nested list, then iterate like a plain array.
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        def flatten(nested):
+            for ele in nested:
+                if ele.isInteger():
+                    self.numbers.append(ele.getInteger())
+                else:
+                    flatten(ele.getList())
+        self.numbers = []
+        self.idx = 0
+        flatten(nestedList)
+        self.lth = len(self.numbers)
+    
+    def next(self) -> int:
+        output = self.numbers[self.idx]
+        self.idx += 1
+        return output
+        
+    def hasNext(self) -> bool:
+        return self.idx < self.lth
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
