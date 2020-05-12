41fc65381373a452fbfa40aadf54fc205b810819
xmy7080 <ximuyuan1989@gmail.com>
2020-05-11 18:38:51 -0700

    Update FindMedianFromDataStream.py

--- FindMedianFromDataStream.py
+++ FindMedianFromDataStream.py
@@ -1,3 +1,40 @@
+#lt solution using two heap, difference between two version is, latest one actually pushed in a tuple in lo heap
+#cause in python we need reverse all number in min heap to form a max heap
+from heapq import heapify,heappush,heappop
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.lo = [] #max heap
+        self.hi = [] #min heap
+        heapify(self.lo)
+        heapify(self.hi)
+        
+
+    def addNum(self, num: int) -> None:
+        heappush(self.lo, (-num, num))
+        heappush(self.hi, heappop(self.lo)[1])
+        
+        while len(self.lo) < len(self.hi):
+            minInHi = heappop(self.hi)
+            heappush(self.lo, (-minInHi, minInHi))
+
+    def findMedian(self) -> float:
+        if len(self.lo) > len(self.hi): 
+            return self.lo[0][1] 
+        else:
+            return (self.lo[0][1] + self.hi[0]) * 0.5
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+#============
+#old version
 class MedianFinder(object):
 
     def __init__(self):
