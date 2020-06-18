55a14dd35fbdaa6a9dbe022fc9537199823514ef
xmy7080 <ximuyuan1989@gmail.com>
2020-06-18 12:58:32 -0700

    Update IntegertoEnglishWords.py

--- IntegertoEnglishWords.py
+++ IntegertoEnglishWords.py
@@ -1,3 +1,37 @@
+#selfmade solution, using string concatenante at the end
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        if not num:
+            return 'Zero'
+        ths = [None, 'Thousand', 'Million', 'Billion']
+        oneToTeens = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+        tens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+        def getThousand(num) -> [str]:
+            tmp = []
+            lessHundred = num % 100
+            if lessHundred < 20:
+                tmp = [oneToTeens[lessHundred-1]] + tmp if lessHundred else tmp
+            else:
+                tenCounts = lessHundred // 10
+                oneCounts = lessHundred % 10
+                tmp = [oneToTeens[oneCounts-1]] + tmp if oneCounts else tmp
+                tmp = [tens[tenCounts-2]] + tmp
+            handredCounts = num // 100
+            tmp = [oneToTeens[handredCounts-1], 'Hundred'] + tmp if handredCounts else tmp
+            return tmp
+                
+        ans = []
+        thCounts = 0
+        while num:
+            thd = num % 1000
+            thousandList = getThousand(thd)
+            ans = [ths[thCounts]] + ans if thCounts and thousandList else ans
+            ans = thousandList + ans
+            num = num // 1000
+            thCounts += 1
+        return ' '.join(ans)
+#================
+
 LESS_THAN_20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
 TENS = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
 THOUSANDS = ["", "Thousand", "Million", "Billion"]
