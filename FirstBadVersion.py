f5f62e04dc88549b8de5943be95b45d936178f8c
xmy7080 <ximuyuan1989@gmail.com>
2020-06-19 15:44:29 -0700

    Update FirstBadVersion.py

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -1,5 +1,27 @@
 # The isBadVersion API is already defined for you.
 # @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l, r = 0, n
+        while l <= r:
+            mid = (l+r)//2
+            if isBadVersion(mid):
+                if not isBadVersion(mid - 1):
+                    return mid
+                r = mid-1
+            else:
+                l = mid + 1
+        
+#==========
+# The isBadVersion API is already defined for you.
+# @param version, an integer
 # @return a bool
 # def isBadVersion(version):
 
