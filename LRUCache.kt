68315f815b8440a9555c912c9a6d15d582f5a791
xmy7080 <ximuyuan1989@gmail.com>
2024-06-03 22:09:48 -0700

    Create LRUCache.kt

---
+++ LRUCache.kt
@@ -0,0 +1,69 @@
+class LRUCache(val capacity: Int) {
+    private var map = mutableMapOf<Int, LinkedNode>()
+    private var firstNode = LinkedNode(-1, -1)
+    private var lastNode = firstNode
+    // private val capacity = capacity
+
+    fun get(key: Int): Int {
+        // val node = map[key] ?: return -1
+        // delete(node)
+        // append(node)
+        // return node.value
+        if (key in map){
+            val node = map[key]
+            delete(node!!)
+            append(node!!)
+            return node!!.value
+        }
+        else {
+            return -1
+        }
+    }
+
+    fun put(key: Int, value: Int) {
+        if (key in map){
+            delete(map[key]!!)
+            append(LinkedNode(key, value))
+        }
+        else if (map.size == capacity) {
+            delete(firstNode.right!!)
+            append(LinkedNode(key, value))
+        }
+        else {
+            append(LinkedNode(key, value))
+        }
+    }
+    private fun append(node: LinkedNode) {
+        lastNode.right = node
+        node.left = lastNode
+        lastNode = node
+        map[node.key] = node
+    }
+    private fun delete(node: LinkedNode) {
+        node.left!!.right = node.right
+        node.right?.let {it.left = node.left!!}
+        map -= node.key
+
+// last node
+        if(lastNode == node) {
+            lastNode = node.left!!
+        }
+
+        node.left = null
+        node.right = null
+    }
+
+    private class LinkedNode(
+        val key: Int,
+        val value: Int,
+        var left: LinkedNode? = null,
+        var right: LinkedNode? = null
+        )
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
