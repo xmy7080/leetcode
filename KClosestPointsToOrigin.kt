17dea293105f28b212c7f78f1cc7281df0310edd
xmy7080 <ximuyuan1989@gmail.com>
2024-06-09 11:54:16 -0700

    Create KClosestPointsToOrigin.kt

---
+++ KClosestPointsToOrigin.kt
@@ -0,0 +1,22 @@
+// to find Kth largest => hold k-1 elements in the base of of the min heap, then the top is the Kth largest
+//                     => OR, put all elements in a max heap and pop k-1 times, then the peak is the Kth Largest
+// to find Kth smallest => holde k-1 elements in the base of the max heap, then the top is the Kth smallest
+//                      => OR, put all elments in a min heap, then pop k-1 times, then the peak would be Kth smallest
+class Solution {
+    fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
+        val compareByDistance: Comparator<IntArray> = compareBy {distance(it)}
+        var pq = PriorityQueue<IntArray>(compareByDistance)
+        points.forEach{ point ->
+            pq.add(point)
+        }
+        var result = mutableListOf<IntArray>()
+        for (i in 0 until k) {
+            result += pq.poll()
+        }
+        return result.toTypedArray()
+    }
+
+    fun distance(point: IntArray): Int {
+        return point[0] * point[0] + point[1] * point[1]
+    }
+}
