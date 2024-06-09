0ac816441322344cc9166d03ab8ba7f3bce9fa05
xmy7080 <ximuyuan1989@gmail.com>
2024-06-09 12:02:29 -0700

    Update KClosestPointsToOrigin.kt

--- KClosestPointsToOrigin.kt
+++ KClosestPointsToOrigin.kt
@@ -3,17 +3,29 @@
 // to find Kth smallest => holde k-1 elements in the base of the max heap, then the top is the Kth smallest
 //                      => OR, put all elments in a min heap, then pop k-1 times, then the peak would be Kth smallest
 class Solution {
+    // solution with min heap
+    // fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
+    //     val compareByDistance: Comparator<IntArray> = compareBy {distance(it)}
+    //     var pq = PriorityQueue<IntArray>(compareByDistance)
+    //     points.forEach{ point ->
+    //         pq.add(point)
+    //     }
+    //     var result = mutableListOf<IntArray>()
+    //     for (i in 0 until k) {
+    //         result += pq.poll()
+    //     }
+    //     return result.toTypedArray()
+    // }
+
+// solution with max heap
     fun kClosest(points: Array<IntArray>, k: Int): Array<IntArray> {
-        val compareByDistance: Comparator<IntArray> = compareBy {distance(it)}
+        val compareByDistance: Comparator<IntArray> = compareBy { -1 * distance(it)}
         var pq = PriorityQueue<IntArray>(compareByDistance)
         points.forEach{ point ->
             pq.add(point)
+            if (pq.size > k) pq.poll()
         }
-        var result = mutableListOf<IntArray>()
-        for (i in 0 until k) {
-            result += pq.poll()
-        }
-        return result.toTypedArray()
+        return pq.toTypedArray()
     }
 
     fun distance(point: IntArray): Int {
