0c492854a86a34f78b88f2cb475417d632adfc36
xmy7080 <ximuyuan1989@gmail.com>
2024-06-10 17:41:57 -0700

    Update randomPickIndex.py

--- randomPickIndex.py
+++ randomPickIndex.py
@@ -20,10 +20,34 @@ class Solution(object):
                 continue
             count += 1
             if random.randrange(count) == 0:
-                res = i
+                res = i # this is not guarantee to be hit, therefore -1 may be returned for a actual existing value
         return res
         
+# 
+# a better solution should be
+def __init__(self, nums: List[int]):
+    self.nums = nums
 
+def pick(self, target: int) -> int:
+    cnt = idx = 0
+    for i, num in enumerate(self.nums):
+        if num != target:
+            continue
+        if cnt == 0:
+            idx = i
+            cnt = 1
+        else:
+            # this random will already give me numbers
+            # between 0 and cnt inclusive
+            # so for 2nd number I am getting random number 0 and 1
+            # so each having a probability of 1/2
+            # similarly for three numbers it will be 1/3
+            rnd = random.randint(0, cnt)
+            if (rnd == cnt):
+                idx = i
+            cnt += 1
+    
+    return idx
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
