aa5d3f50833e7ee1cc458c4b0153973fac9e9aca
xmy7080 <ximuyuan1989@gmail.com>
2024-07-05 23:06:52 -0700

    Create meetingRoomsII.kt

---
+++ meetingRoomsII.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun minMeetingRooms(intervals: Array<IntArray>): Int {
+        val times = mutableListOf<Pair<Int, Int>>()
+        intervals.forEach{itv ->
+            times.add(Pair(itv[0], 1))
+            times.add(Pair(itv[1], -1))
+        }
+        val countable = times.sortedWith(compareBy ({it.first}, {it.second}))
+        var rooms = 0
+        var result = 0
+        countable.forEach{time ->
+            rooms += time.second
+            result = maxOf(result, rooms)
+        }
+        // println("countable is " + countable.map{"time " + it.first + " in/out " + it.second}.joinToString("\n"))
+        return result
+    }
+}
