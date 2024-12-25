3e1ccda1e49388773a4669cddc5498601e6911e8
xmy7080 <ximuyuan1989@gmail.com>
2024-12-24 23:20:38 -0800

    Update dailyTemperatures.kt

--- dailyTemperatures.kt
+++ dailyTemperatures.kt
@@ -1,17 +1,33 @@
 //use of monotonic stack
+data class TempIdx(
+    val temp: Int,
+    val idx: Int
+)
 class Solution {
     fun dailyTemperatures(temperatures: IntArray): IntArray {
-        val stack = ArrayDeque<IntArray>()
-        val res = IntArray(temperatures.size){0}
+        // use of data class
+        val stack = ArrayDeque<TempIdx>()
+        val result = IntArray(temperatures.size){0}
         for(i in temperatures.size -1 downTo 0){
-            while(stack.size > 0 && stack.first()!![0] <= temperatures[i]) {
+            while(stack.size > 0 && stack.first()!!.temp <= temperatures[i]){
                 stack.removeFirst()
             }
-            if(!stack.isEmpty()){
-                res[i] = stack.first()!![1] - i
-            }
-            stack.addFirst(intArrayOf(temperatures[i], i))
+            if(stack.isNotEmpty()) result[i] = stack.first()!!.idx - i
+            stack.addFirst(TempIdx(temperatures[i], i) )
         }
-        return res
+        return result
+
+        // val stack = ArrayDeque<IntArray>()
+        // val res = IntArray(temperatures.size){0}
+        // for(i in temperatures.length-1 downTo 0){
+        //     while(stack.size > 0 && stack.first()!![0] < temperatures[i]) {
+        //         stack.removeFirst()
+        //     }
+        //     if(stack.first() != null){
+        //         res[i] = stack.first()!![1] - i
+        //     }
+        //     stack.addFirst(intArrayOf(temperature[i], i))
+        // }
+        // return res
     }
 }
