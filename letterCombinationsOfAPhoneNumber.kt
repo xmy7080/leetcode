57cfec98ab060c1a8424a1df13f7cd47a15752fd
xmy7080 <ximuyuan1989@gmail.com>
2025-02-17 17:54:48 -0800

    Update letterCombinationsOfAPhoneNumber.kt

--- letterCombinationsOfAPhoneNumber.kt
+++ letterCombinationsOfAPhoneNumber.kt
@@ -3,17 +3,17 @@ class Solution {
     val keyMap: Map<Char,String> = mapOf('2' to "abc", '3' to "def", '4' to "ghi", '5' to "jkl", '6' to "mno", '7' to "pqrs", '8' to "tuv", '9' to "wxyz" )
     fun letterCombinations(digits: String): List<String> {
         val res = mutableListOf<String>()
-        fun dfs(temp: StringBuilder, i: Int) {
-            var tmp = temp
+        fun dfs(tmp: StringBuilder, i: Int) {
+            // var tmp = temp
             if(i == digits.length) {
                 res.add(tmp.toString())
                 return
             }
             val chars = keyMap[digits[i]] ?: ""
             for (c in chars) {
-                tmp = tmp.append(c)
+                tmp.append(c) // used to be tmp = tmp.append(c), but turns out append will just change string builder object directly
                 dfs(tmp, i+1)
-                tmp = tmp.deleteCharAt(tmp.length - 1)
+                tmp.deleteCharAt(tmp.length - 1) // used to be tmp = tmp.deleteCharAt(), same reason with above
             }
         }
         if(digits.length == 0) return res
