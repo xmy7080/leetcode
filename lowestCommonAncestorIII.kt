7501feda62086f4a7017ba4882f500d702e266e6
xmy7080 <ximuyuan1989@gmail.com>
2025-06-02 16:15:00 -0700

    Update lowestCommonAncestorIII.kt

--- lowestCommonAncestorIII.kt
+++ lowestCommonAncestorIII.kt
@@ -1,3 +1,25 @@
+// optimized way doing this, solution refers to approach 3 in https://leetcode.com/problems/intersection-of-two-linked-lists/editorial/
+/**
+ * Definition for a Node.
+ * class Node(var `val`: Int) {
+ *		var left: TreeNode? = null
+ *		var right: TreeNode? = null
+ *		var parent: Node? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(p: Node?, q: Node?): Node? {
+        var a = p
+        var b = q
+        while(a != b){
+            a = a?.parent ?: q
+            b = b?.parent ?: p
+        }
+        return a
+    }
+}
+// naive hashset way of doing this
 /**
  * Definition for a Node.
  * class Node(var `val`: Int) {
